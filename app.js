d4a3c33fd06d9c6da821f9070a9a90e18a206732
Brian <briguy22811@gmail.com>
2018-06-27 21:59:14 -0400

    Added looping through transactions returned from etherscan

--- app.js
+++ app.js
@@ -7,24 +7,40 @@ var key = process.env.etherscanAPI;
 var address = process.env.ethAddress;
 
 app.get('/', function (req, res) {
- 	res.send('Welcome to FeedVitalik! For just 1 Wei () you can help feed a starving Vitalik...will you be the hero the crypto world needs?');
+ 	res.send('Welcome to FeedVitalik! Help feed a starving Vitalik...will you be the hero the crypto world needs?');
 
  	var balanceUrl = 'https://api.etherscan.io/api?module=account&action=balance&address='+address+'&tag=latest&apikey='+key;
  	var txUrl = 'https://api.etherscan.io/api?module=account&action=txlist&address='+address+'&tag=latest&apikey='+key;
  	
  	// GET Balance
 	request(balanceUrl, function (error, response, body) {
-	    if (!error && response.statusCode == 200) {
-	        console.log("Balance request: " + body)
-	     }
-	})
+		if (!error && response.statusCode == 200) {
+		    console.log("Balance request: " + body)
+		}
+	});
 
-	//GET Tx
-	request(txUrl, function (error, response, body) {
-	    if (!error && response.statusCode == 200) {
-	        console.log("Tx request: " + body)
-	     }
-	})
+	
+	// GET Tx every 5 seconds
+	// Only print new Tx
+	var prevLength = 0;
+	setInterval(function(){
+		request(txUrl, function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		    	var responseObj = JSON.parse(body);
+		    	var length = responseObj.result.length;
+		    	console.log("Previous: " + prevLength);
+		    	console.log("Length: " + length);
+		    	
+		    	if(length > prevLength){
+			    	for (i=0; i < responseObj.result.length; i++){
+			    		// Transactions are in order from newest to oldest
+			    		res.write("Tx Item "+i+": " + responseObj.result[i].blockNumber);
+			    		prevLength = length;
+			    	}
+			    }
+		    }
+		});
+	}, 5000);
 
 });
 
