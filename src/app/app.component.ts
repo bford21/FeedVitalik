1ff2c5f4afe625fb7bc3ec05d6135998712e0331
Brian <brianfordp@gmail.com>
2020-07-19 21:13:46 -0400

    Falling eth, vitalik movement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
-import { Observable, interval } from 'rxjs';
-import Web3 from 'web3';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,36 +6,10 @@ import Web3 from 'web3';
 })
 export class AppComponent implements OnInit {
   title = 'feedvitalik';
-  latestBlock = 0;
-  mostRecentTransactions: Array<any>;
-  web3: any;
 
   constructor() {}
 
     ngOnInit() {
-      // Project ID
-      // this.web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/'));
-
-      // const data = interval(5000).subscribe((x => {
-      //   this.web3.eth.getBlockNumber().then(result => {
-      //     if (result > this.latestBlock) {
-      //       this.latestBlock = result;
-      //       this.getBlock(result);
-      //     }
-      //     console.log(this.latestBlock);
-      //   },
-      //   msg => {
-      //     // reject(msg);
-      //   });
-      // }));
 
     }
-
-    getBlock(block: any) {
-      this.web3.eth.getBlock(block).then(result => {
-        this.mostRecentTransactions = result.transactions;
-        console.log('Transactions: ', this.mostRecentTransactions);
-      });
-    }
-
 }
--- src/app/models/eth.ts
+++ src/app/models/eth.ts
@@ -1,25 +1,84 @@
 export class Eth {
     private x = 0;
-    private y = 0;
+    public y = -10.0;
     // Eventually these will be randomized
     private width = 100;
     private height = 100;
+    private speed;
     private eth = new Image();
-    private ethSrc = '../../assets/Images/vitalikSmile_Transparent.png';
+    private ethXSSrc = '../../assets/Images/eth_xs.png';
+    private ethSmallSrc = '../../assets/Images/eth_small.png';
+    private ethMedSrc = '../../assets/Images/eth_medium.png';
+    private ethLargeSrc = '../../assets/Images/eth_large.png';
+    private ethXLSrc = '../../assets/Images/eth_xl.png';
+    private ethXXLSrc = '../../assets/Images/eth_xl.png';
 
-    constructor(private context: CanvasRenderingContext2D) {}
+    constructor(private context: CanvasRenderingContext2D, transaction, canvasWidth) {
+        this.setSize(transaction.value);
+        this.setSpeed(transaction.gas);
+        this.x = this.generateRandom(0.0, canvasWidth);
+    }
 
-    moveRight() {
-      this.y--;
-      this.draw();
+    moveDown() {
+        this.y = this.y + this.speed;
+        this.draw();
+    }
+
+    // min included, max excluded
+    private generateRandom(min, max) {
+        return Math.random() < 0.5 ? (( 1 - Math.random()) * (max - min) + min) : (Math.random() * (max - min) + min);
+    }
+
+    private setSize(value) {
+        // Less than .1 eth
+        if (value < 100000000000000000) {
+            this.width = 50;
+            this.height = 50;
+            this.eth.src = this.ethXSSrc;
+        } else if (value < 1000000000000000000) {
+            // less than 1 eth
+            this.width = 100;
+            this.height = 100;
+            this.eth.src = this.ethSmallSrc;
+        } else if (value < 10000000000000000000) {
+            // less than 10 eth
+            this.width = 150;
+            this.height = 150;
+            this.eth.src = this.ethMedSrc;
+        } else if (value < 100000000000000000000) {
+            // less than 100 eth
+            this.width = 200;
+            this.height = 200;
+            this.eth.src = this.ethLargeSrc;
+        } else if (value < 1000000000000000000000) {
+            // less than 1000 eth
+            this.width = 250;
+            this.height = 250;
+            this.eth.src = this.ethXLSrc;
+        } else {
+            // Greater than 1000
+            this.width = 350;
+            this.height = 350;
+            this.eth.src = this.ethXXLSrc;
+        }
+    }
+
+    private setSpeed(gas) {
+        if (gas < 20000) {
+            this.speed = this.generateRandom(0.5, 1.0);
+        } else if (gas < 50000) {
+            this.speed = this.generateRandom(1.0, 2.0);
+        } else if (gas < 100000) {
+            this.speed = this.generateRandom(2.0, 3.0);
+        } else if (gas < 200000) {
+            this.speed = this.generateRandom(3.0, 4.0);
+        } else {
+            this.speed = this.generateRandom(4.0, 5.0);
+        }
+        console.log(this.speed);
     }
 
     private draw() {
-        this.eth.src = this.ethSrc;
-        this.eth.onload = (() => {
-            if (this.eth.src.length > 0) {
-                this.context.drawImage(this.eth, this.x, this.y, this.width, this.height);
-            }
-        });
+        this.context.drawImage(this.eth, this.x, this.y, this.width, this.height);
     }
 }
